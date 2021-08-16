9b8f65afd395c9bbf495583c716cecdfb66d6f6b
zdx <1192132727@qq.com>
2021-08-16 19:30:35 +0800

    提交

--- Algorithm/sortColors.java
+++ Algorithm/sortColors.java
@@ -1,3 +1,18 @@
-public class sortColors {
-    
+class Solution {
+    public void sortColors(int[] nums) {
+        int zero = -1, one = 0, two = nums.length;
+        while (one < two) {
+            if(nums[one] == 0)
+                swap(nums, ++zero, one++);
+            else if(nums[one] == 2)
+                swap(nums, --two, one);
+            else
+                ++one;
+        }
+    }
+    private void swap(int[] nums, int i, int j){
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
 }
