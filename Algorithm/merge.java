c17886549f09731e33a6b5ba86a9f07d20201fa6
zdx <1192132727@qq.com>
2021-08-18 22:52:00 +0800

    提交

--- Algorithm/merge.java
+++ Algorithm/merge.java
@@ -22,7 +22,7 @@ class Solution {
 }
 
 public class merge {
-    public static int[] stringToIntegerArray(String input) {
+    public static int[] stringToIntegerArray(String input) {    
         input = input.trim();
         input = input.substring(1, input.length() - 1);
         if (input.length() == 0) {
