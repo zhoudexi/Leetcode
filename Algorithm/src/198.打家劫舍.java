260281f2e4eefcbac2174f80615ae53b696b226d
zdx <1192132727@qq.com>
2021-09-05 22:52:48 +0800

    提交

---
+++ Algorithm/src/198.打家劫舍.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=java
+ *
+ * [198] 打家劫舍
+ */
+
+// @lc code=start
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[nums.length - 1];
+    }
+}
+// @lc code=end
+
