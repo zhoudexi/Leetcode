3e234be4198089720b8b58b8de7f8d31dcd81bd8
zdx <1192132727@qq.com>
2021-09-09 22:27:02 +0800

    提交

---
+++ Algorithm/src/303.区域和检索-数组不可变.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=java
+ *
+ * [303] 区域和检索 - 数组不可变
+ */
+
+// @lc code=start
+class NumArray {
+    private int[] sums;
+    public NumArray(int[] nums) {
+        sums = new int[nums.length + 1];
+        for (int i = 1; i <= nums.length; i++) {
+            sums[i] = sums[i - 1] + nums[i - 1]; 
+        }
+    }
+    
+    public int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
+// @lc code=end
+
--- Algorithm/src/64.最小路径和.java
+++ Algorithm/src/64.最小路径和.java
@@ -6,27 +6,25 @@
 
 // @lc code=start
 class Solution {
-    private int[][] direction = {{1,0}, {0,-1}};
-    int m, n;
     public int minPathSum(int[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
-        int min = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                min = Math.min(min, dfs(grid, i, j));
+        if(grid == null || grid.length == 0 || grid[0].length == 0)
+            return 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
             }
         }
-        return min;
-    }
-    private int dfs(int[][] grid, int i, int j) {
-        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0)
-            return 0;
-        grid[i][j] = 0;
-        int sum = 0;
-        for(int[] d : direction)
-            sum += dfs(grid, i + d[0], j + d[1]);
-        return sum;
+        return dp[m - 1][n- 1];
     }
 }
 // @lc code=end
