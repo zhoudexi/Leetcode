5ce29ad21e6736f9005fc9597475ffeca314190e
zdx <1192132727@qq.com>
2021-10-06 22:38:36 +0800

    提交

---
+++ Algorithm/src/____Offer_24______LCOF/Solution.java
@@ -0,0 +1,26 @@
+// @algorithm @lc id=100298 lang=java 
+// @title fan-zhuan-lian-biao-lcof
+
+
+package ____Offer_24______LCOF;
+import algm.*;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head, pre = null;
+        while(cur != null){
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+}
