2811a9a7d6d5eab9f946c716d8f83b323464406c
zdx <1192132727@qq.com>
2021-10-10 22:53:35 +0800

    提交

--- Algorithm/src/____Offer_33______________LCOF/Solution.java
+++ Algorithm/src/____Offer_33______________LCOF/Solution.java
@@ -5,6 +5,15 @@
 package ____Offer_33______________LCOF;
 public class Solution {
     public boolean verifyPostorder(int[] postorder) {
+        return recur(postorder, 0, postorder.length - 1);
+    }
 
+    private boolean recur(int[] postorder, int i, int j) {
+        if(i >= j) return true;
+        int p = i;
+        while(postorder[p] < postorder[j]) p++;
+        int m = p;
+        while(postorder[p] > postorder[j]) p++;
+        return p == j && recur(postorder, i, m - 1) && recur(postorder, m, j - 1);
     }
 }
