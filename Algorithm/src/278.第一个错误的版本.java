2bcd344ff415127e1c741445278491213be71786
zhoudexi <1192132727@qq.com>
2021-10-30 22:33:58 +0800

    提交

---
+++ Algorithm/src/278.第一个错误的版本.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=278 lang=java
+ *
+ * [278] 第一个错误的版本
+ */
+
+// @lc code=start
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        
+    }
+}
+// @lc code=end
+
---
+++ Algorithm/src/540.有序数组中的单一元素.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=540 lang=java
+ *
+ * [540] 有序数组中的单一元素
+ */
+
+// @lc code=start
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+
+    }
+}
+// @lc code=end
+
