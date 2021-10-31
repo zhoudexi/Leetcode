f1dbd248f25a61e94bb2bebc5440dad027dd2849
zhoudexi <1192132727@qq.com>
2021-10-31 22:33:43 +0800

    提交

--- Algorithm/src/455.分发饼干.java
+++ Algorithm/src/455.分发饼干.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
  * @lc app=leetcode.cn id=455 lang=java
  *
@@ -7,7 +9,14 @@
 // @lc code=start
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int gi = 0, si = 0;
+        while(gi < g.length && si < s.length){
+            if(g[gi] <= s[si]) gi++;
+            si++;
+        }
+        return gi;
     }
 }
 // @lc code=end
