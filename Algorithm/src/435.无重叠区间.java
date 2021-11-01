ecde1e642d48b2608b0d823e6861b9c5c08e8383
zhoudexi <1192132727@qq.com>
2021-11-01 22:50:55 +0800

    提交

--- Algorithm/src/435.无重叠区间.java
+++ Algorithm/src/435.无重叠区间.java
@@ -7,7 +7,26 @@
 // @lc code=start
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        return 0;
+        if (intervals.length == 0) {
+            return 0;
+        }
+        
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] interval1, int[] interval2) {
+                return interval1[1] - interval2[1];
+            }
+        });
+
+        int n = intervals.length;
+        int right = intervals[0][1];
+        int ans = 1;
+        for (int i = 1; i < n; ++i) {
+            if (intervals[i][0] >= right) {
+                ++ans;
+                right = intervals[i][1];
+            }
+        }
+        return n - ans;
     }
 }
 // @lc code=end
