b6a0cdca033bcf370d21a84b6e83833f17add5a9
zhoudexi <1192132727@qq.com>
2021-11-05 22:55:12 +0800

    提交

---
+++ Algorithm/src/392.判断子序列.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=392 lang=java
+ *
+ * [392] 判断子序列
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int index = -1;
+        for (char c : s.toCharArray()) {
+            index = t.indexOf(c, index + 1);
+            if(index == -1) return false;
+        }
+        return true; 
+    }
+}
+// @lc code=end
+
---
+++ Algorithm/src/665.非递减数列.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=665 lang=java
+ *
+ * [665] 非递减数列
+ */
+
+// @lc code=start
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        
+        return false;
+    }
+}
+// @lc code=end
+
