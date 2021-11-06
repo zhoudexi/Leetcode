8fc193e1f747d230f706647c5856e6930b51dc85
zhoudexi <1192132727@qq.com>
2021-11-06 22:42:58 +0800

    提交

--- Algorithm/src/665.非递减数列.java
+++ Algorithm/src/665.非递减数列.java
@@ -7,9 +7,20 @@
 // @lc code=start
 class Solution {
     public boolean checkPossibility(int[] nums) {
-        
-        return false;
+    int cnt = 0;
+    for (int i = 1; i < nums.length && cnt < 2; i++) {
+        if (nums[i] >= nums[i - 1]) {
+            continue;
+        }
+        cnt++;
+        if (i - 2 >= 0 && nums[i - 2] > nums[i]) {
+            nums[i] = nums[i - 1];
+        } else {
+            nums[i - 1] = nums[i];
+        }
     }
+    return cnt <= 1;
+}
 }
 // @lc code=end
 
