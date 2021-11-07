b1829c3c3aeb5eefcf1410d02690d50d33155536
zhoudexi <1192132727@qq.com>
2021-11-07 22:20:04 +0800

    提交

---
+++ Algorithm/src/763.划分字母区间.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=763 lang=java
+ *
+ * [763] 划分字母区间
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int[] last = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            last[s.charAt(i) - 'a'] = i;
+        }
+        List<Integer> list = new ArrayList<>();
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, last[s.charAt(i) - 'a']);
+            if(i == end){
+                list.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return list;
+    }
+}
+// @lc code=end
+
