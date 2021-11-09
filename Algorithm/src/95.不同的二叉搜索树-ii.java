32e18dac901666d2a70824487ea443985d9a055c
zhoudexi <1192132727@qq.com>
2021-11-09 23:01:19 +0800

    提交

--- Algorithm/src/95.不同的二叉搜索树-ii.java
+++ Algorithm/src/95.不同的二叉搜索树-ii.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 /*
  * @lc app=leetcode.cn id=95 lang=java
  *
@@ -22,8 +25,30 @@
  */
 class Solution {
     public List<TreeNode> generateTrees(int n) {
-        
+        if(n < 1) return new LinkedList<TreeNode>();
+        return generateSubtrees(1, n);
+    }
+    private List<TreeNode> generateSubtrees(int s, int e){
+        List<TreeNode> res = new LinkedList<>();
+        if(s > e){
+            res.add(null);
+            return res;
+        }
+        for (int i = s; i <= e; i++) {
+            List<TreeNode> left = generateSubtrees(s, i - 1);
+            List<TreeNode> right = generateSubtrees(i + 1, e);
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
     }
 }
+
 // @lc code=end
 
