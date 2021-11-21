613ce0c0e117a2af2068cbda3b501e81bc66ea52
zhoudexi <1192132727@qq.com>
2021-11-21 22:55:52 +0800

    提交

--- Algorithm/src/1.两数之和.java
+++ Algorithm/src/1.两数之和.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /*
  * @lc app=leetcode.cn id=1 lang=java
  *
@@ -7,9 +9,15 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-      
-		    return nums;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(hashMap.containsKey(target - nums[i])){
+                return new int[]{hashMap.get(target - nums[i]), i};
+            }else{
+                hashMap.put(nums[i], i);
+            }
+        }
+	    return null;
     }
 }
 // @lc code=end
