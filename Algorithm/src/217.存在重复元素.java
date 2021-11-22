98131cd3e58b456b6eb6326bbe96a0df0f43087e
zhoudexi <1192132727@qq.com>
2021-11-22 22:01:33 +0800

    提交

---
+++ Algorithm/src/217.存在重复元素.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Set;
+
+/*
+ * @lc app=leetcode.cn id=217 lang=java
+ *
+ * [217] 存在重复元素
+ */
+
+// @lc code=start
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            set.add(i);
+        }
+        return set.size() < nums.length;
+    }
+}
+// @lc code=end
+
