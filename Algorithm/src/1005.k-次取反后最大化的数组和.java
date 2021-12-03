dbcf5fc8a654d40d456058a8a89e8d39d0cf7417
zhoudexi <1192132727@qq.com>
2021-12-03 23:45:41 +0800

    提交

---
+++ Algorithm/src/1005.k-次取反后最大化的数组和.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=1005 lang=java
+ *
+ * [1005] K 次取反后最大化的数组和
+ */
+
+// @lc code=start
+class Solution {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        int n = nums.length, idx = 0;
+        PriorityQueue<Integer> q = new PriorityQueue<>((a,b)->nums[a]-nums[b]);
+        boolean zero = false;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) q.add(i);
+            if (nums[i] == 0) zero = true;
+            if (Math.abs(nums[i]) < Math.abs(nums[idx])) idx = i;
+        }
+        if (k <= q.size()) {
+            while (k-- > 0) nums[q.peek()] = -nums[q.poll()];
+        } else {
+            while (!q.isEmpty() && k-- > 0) nums[q.peek()] = -nums[q.poll()];
+            if (!zero && k % 2 != 0) nums[idx] = -nums[idx];
+        }
+        int ans = 0;
+        for (int i : nums) ans += i;
+        return ans;
+    }
+}
+
+// @lc code=end
+
