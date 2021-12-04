2ae30a3b56f8621a89564e7ef78bda3b64f2bd00
zhoudexi <1192132727@qq.com>
2021-12-04 23:55:04 +0800

    提交

---
+++ Algorithm/src/383.赎金信.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=383 lang=java
+ *
+ * [383] 赎金信
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] cnt = new int[26];
+        for (char c : magazine.toCharArray()) {
+            cnt[c - 'a']++;
+        }
+        for (char c : ransomNote.toCharArray()) {
+            cnt[c - 'a']--;
+            if(cnt[c - 'a'] < 0) return false;
+        }
+        return true;
+    }
+}
+// @lc code=end 
+
