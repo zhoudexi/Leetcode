dfec4f24850ddae1ef1eb87afc7b1aa0c44ae8ef
zhoudexi <1192132727@qq.com>
2021-12-11 00:07:03 +0800

    提交

---
+++ Algorithm/src/748.最短补全词.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=748 lang=java
+ *
+ * [748] 最短补全词
+ */
+
+// @lc code=start
+class Solution {
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        int[] cnt = getCnt(licensePlate);
+        String ans = null;
+        for (String s : words) {
+            int[] cur = getCnt(s);
+            boolean ok = true;
+            for (int i = 0; i < 26 && ok; i++) {
+                if (cnt[i] > cur[i]) ok = false;
+            }
+            if (ok && (ans == null || ans.length() > s.length())) ans = s;
+        }
+        return ans;
+    }
+    int[] getCnt(String s) {
+        int[] cnt = new int[26];
+        for (char c : s.toCharArray()) {
+            if (Character.isLetter(c)) cnt[Character.toLowerCase(c) - 'a']++;
+        }
+        return cnt;
+    }
+}
+
+// @lc code=end
+
