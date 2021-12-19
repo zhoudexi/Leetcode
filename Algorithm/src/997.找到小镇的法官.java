5c6dc103f1779394ba68f988a979bf0d74abd03f
zhoudexi <1192132727@qq.com>
2021-12-20 00:03:23 +0800

    提交

---
+++ Algorithm/src/997.找到小镇的法官.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=997 lang=java
+ *
+ * [997] 找到小镇的法官
+ */
+
+// @lc code=start
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] in = new int[n + 1], out = new int[n + 1];
+        for (int[] t : trust) {
+            int a = t[0], b = t[1];
+            in[b]++; out[a]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (in[i] == n - 1 && out[i] == 0) return i;
+        }
+        return -1;
+    }
+}
+
+// @lc code=end
+
