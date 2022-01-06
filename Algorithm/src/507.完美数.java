086e36e5ae645899175aa7230ea4930a33654a69
zhoudexi <1192132727@qq.com>
2022-01-06 23:53:53 +0800

    提交

--- Algorithm/src/507.完美数.java
+++ Algorithm/src/507.完美数.java
@@ -10,7 +10,7 @@ class Solution {
         if(num == 1) return false;
         int sum = 1;
         for (int i = 2; i <= num / i; i++) {
-            if(num % i == 0){
+            if(num % i == 0){   
                 sum += i;
                 if(i * i != num) sum += num / i;
             }
