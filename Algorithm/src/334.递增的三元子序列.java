f6b60d86a9268dc8543eabcf27e1e69f90a36e6b
zhoudexi <1192132727@qq.com>
2022-01-12 23:57:14 +0800

    提交

---
+++ Algorithm/src/334.递增的三元子序列.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=334 lang=java
+ *
+ * [334] 递增的三元子序列
+ */
+
+// @lc code=start
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+        if (n < 3) {
+            return false;
+        }
+        int[] leftMin = new int[n];
+        leftMin[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            leftMin[i] = Math.min(leftMin[i - 1], nums[i]);
+        }
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
+        }
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] > leftMin[i - 1] && nums[i] < rightMax[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
