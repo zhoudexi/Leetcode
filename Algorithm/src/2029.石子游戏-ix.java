f0a3f2b0715d77275b6dc339de5523dacd31bc50
zhoudexi <1192132727@qq.com>
2022-01-20 23:42:16 +0800

    提交

---
+++ Algorithm/src/2029.石子游戏-ix.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=2029 lang=java
+ *
+ * [2029] 石子游戏 IX
+ */
+
+// @lc code=start
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        for (int val : stones) {
+            int type = val % 3;
+            if (type == 0) {
+                ++cnt0;
+            } else if (type == 1) {
+                ++cnt1;
+            } else {
+                ++cnt2;
+            }
+        }
+        if (cnt0 % 2 == 0) {
+            return cnt1 >= 1 && cnt2 >= 1;
+        }
+        return cnt1 - cnt2 > 2 || cnt2 - cnt1 > 2;
+    }
+}
+// @lc code=end
+
