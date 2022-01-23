a31998209a0f505321c57050bfb4838746d198c7
zhoudexi <1192132727@qq.com>
2022-01-23 23:56:17 +0800

    提交

---
+++ Algorithm/src/2034.股票价格波动.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=2034 lang=java
+ *
+ * [2034] 股票价格波动
+ */
+
+// @lc code=start
+class StockPrice {
+    int cur;
+    Map<Integer, Integer> map = new HashMap<>();
+    TreeMap<Integer, Integer> ts = new TreeMap<>();
+    public void update(int timestamp, int price) {
+        cur = Math.max(cur, timestamp);
+        if (map.containsKey(timestamp)) {
+            int old = map.get(timestamp);
+            int cnt = ts.get(old);
+            if (cnt == 1) ts.remove(old);
+            else ts.put(old, cnt - 1);
+        }
+        map.put(timestamp, price);
+        ts.put(price, ts.getOrDefault(price, 0) + 1);
+    }
+    
+    public int current() {
+        return map.get(cur);
+    }
+    
+    public int maximum() {
+        return ts.lastKey();
+    }
+    
+    public int minimum() {
+        return ts.firstKey();
+    }
+}
+
+/**
+ * Your StockPrice object will be instantiated and called as such:
+ * StockPrice obj = new StockPrice();
+ * obj.update(timestamp,price);
+ * int param_2 = obj.current();
+ * int param_3 = obj.maximum();
+ * int param_4 = obj.minimum();
+ */
+// @lc code=end
+
