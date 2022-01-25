84aeebb5910e3c31d787c09848a878acc813b20b
zhoudexi <1192132727@qq.com>
2022-01-25 23:32:35 +0800

    提交

---
+++ Algorithm/src/1688.比赛中的配对次数.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1688 lang=java
+ *
+ * [1688] 比赛中的配对次数
+ */
+
+// @lc code=start
+class Solution {
+    public int numberOfMatches(int n) {
+        return n - 1;
+    }
+}
+// @lc code=end
+
