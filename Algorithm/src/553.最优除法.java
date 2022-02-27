c5dd1dfe4ec198737cefa78586faa0fce5f37cb6
zhoudexi <1192132727@qq.com>
2022-02-27 22:30:34 +0800

    提交

--- Algorithm/src/553.最优除法.java
+++ Algorithm/src/553.最优除法.java
@@ -7,7 +7,17 @@
 // @lc code=start
 class Solution {
     public String optimalDivision(int[] nums) {
-        
+        int n = nums.length;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(nums[i]);
+            if (i + 1 < n) sb.append("/");
+        }
+        if (n > 2) {
+            sb.insert(sb.indexOf("/") + 1, "(");
+            sb.append(")");
+        }
+        return sb.toString();
     }
 }
 // @lc code=end
