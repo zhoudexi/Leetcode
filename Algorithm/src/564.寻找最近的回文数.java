75675250d3547fb0b185ba2cb7ae4e11dab1791e
zhoudexi <1192132727@qq.com>
2022-03-02 22:44:40 +0800

    提交

---
+++ Algorithm/src/564.寻找最近的回文数.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=564 lang=java
+ *
+ * [564] 寻找最近的回文数
+ */
+
+// @lc code=start
+class Solution {
+    public String nearestPalindromic(String n) {
+        long selfNumber = Long.parseLong(n), ans = -1;
+        List<Long> candidates = getCandidates(n);
+        for (long candidate : candidates) {
+            if (candidate != selfNumber) {
+                if (ans == -1 ||
+                    Math.abs(candidate - selfNumber) < Math.abs(ans - selfNumber) ||
+                    Math.abs(candidate - selfNumber) == Math.abs(ans - selfNumber) && candidate < ans) {
+                    ans = candidate;
+                }
+            }
+        }
+        return Long.toString(ans);
+    }
+
+    public List<Long> getCandidates(String n) {
+        int len = n.length();
+        List<Long> candidates = new ArrayList<Long>() {{
+            add((long) Math.pow(10, len - 1) - 1);
+            add((long) Math.pow(10, len) + 1);
+        }};
+        long selfPrefix = Long.parseLong(n.substring(0, (len + 1) / 2));
+        for (long i = selfPrefix - 1; i <= selfPrefix + 1; i++) {
+            StringBuffer sb = new StringBuffer();
+            String prefix = String.valueOf(i);
+            sb.append(prefix);
+            StringBuffer suffix = new StringBuffer(prefix).reverse();
+            sb.append(suffix.substring(len & 1));
+            String candidate = sb.toString();
+            candidates.add(Long.parseLong(candidate));
+        }
+        return candidates;
+    }
+}
+// @lc code=end
+
