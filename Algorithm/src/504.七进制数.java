943acd469dddf6a0d56e23948152628ab1f7277e
zhoudexi <1192132727@qq.com>
2022-03-07 23:15:15 +0800

    提交

---
+++ Algorithm/src/504.七进制数.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=504 lang=java
+ *
+ * [504] 七进制数
+ */
+
+// @lc code=start
+class Solution {
+    public String convertToBase7(int num) {
+        boolean b = num < 0;
+        num = Math.abs(num);
+        StringBuffer s = new StringBuffer();
+        while(num != 0){
+            s.append(num % 7);
+            num /= 7;
+        }
+        if(b){
+            s.append("-");
+        }
+        return s.reverse().toString();
+    }
+}
+// @lc code=end
+
