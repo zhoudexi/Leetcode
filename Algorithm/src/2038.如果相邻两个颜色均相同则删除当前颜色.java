a0895d3c75e212e633e4ddcde442ae798f6b9daa
zhoudexi <1192132727@qq.com>
2022-03-22 23:28:18 +0800

    提交

---
+++ Algorithm/src/2038.如果相邻两个颜色均相同则删除当前颜色.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=2038 lang=java
+ *
+ * [2038] 如果相邻两个颜色均相同则删除当前颜色
+ */
+
+// @lc code=start
+class Solution {
+    public boolean winnerOfGame(String colors) {
+        char[] c = colors.toCharArray();
+        int n = colors.length();
+        int a = 0, b = 0;
+        for(int i = 0; i < n - 1; i++){
+            if(c[i] == 'A' && c[i - 1] == 'A' && c[i + 1] == 'A') a++;
+            if(c[i] == 'B' && c[i - 1] == 'B' && c[i + 1] == 'B') b++;
+        }
+        return a > b;
+    }
+}
+// @lc code=end
+
