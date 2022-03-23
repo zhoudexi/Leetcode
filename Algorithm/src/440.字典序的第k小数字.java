45a7ff9aa04541b14cff443da49189eefa243b32
zhoudexi <1192132727@qq.com>
2022-03-23 23:55:53 +0800

    提交

---
+++ Algorithm/src/440.字典序的第k小数字.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=440 lang=java
+ *
+ * [440] 字典序的第K小数字
+ */
+
+// @lc code=start
+class Solution {
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k--;
+        while (k > 0) {
+            int steps = getSteps(curr, n);
+            if (steps <= k) {
+                k -= steps;
+                curr++;
+            } else {
+                curr = curr * 10;
+                k--;
+            }
+        }
+        return curr;
+    }
+
+    public int getSteps(int curr, long n) {
+        int steps = 0;
+        long first = curr;
+        long last = curr;
+        while (first <= n) {
+            steps += Math.min(last, n) - first + 1;
+            first = first * 10;
+            last = last * 10 + 9;
+        }
+        return steps;
+    }
+}
+// @lc code=end
+
