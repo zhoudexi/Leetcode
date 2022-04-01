138bb28296ce6ec669d97a341a56d859fd31fc33
zhoudexi <1192132727@qq.com>
2022-04-01 23:17:34 +0800

    提交

---
+++ Algorithm/src/954.二倍数对数组.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=954 lang=java
+ *
+ * [954] 二倍数对数组
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canReorderDoubled(int[] arr) {
+        Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+        for (int x : arr) {
+            cnt.put(x, cnt.getOrDefault(x, 0) + 1);
+        }
+        if (cnt.getOrDefault(0, 0) % 2 != 0) {
+            return false;
+        }
+
+        List<Integer> vals = new ArrayList<Integer>();
+        for (int x : cnt.keySet()) {
+            vals.add(x);
+        }
+        Collections.sort(vals, (a, b) -> Math.abs(a) - Math.abs(b));
+
+        for (int x : vals) {
+            if (cnt.getOrDefault(2 * x, 0) < cnt.get(x)) { // 无法找到足够的 2x 与 x 配对
+                return false;
+            }
+            cnt.put(2 * x, cnt.getOrDefault(2 * x, 0) - cnt.get(x));
+        }
+        return true;
+    }
+}
+
+// @lc code=end
+
