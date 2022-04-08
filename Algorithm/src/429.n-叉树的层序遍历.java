b9f5ef401894885d02e255f335ea2757dd74e80d
zhoudexi <1192132727@qq.com>
2022-04-08 23:36:03 +0800

    提交

---
+++ Algorithm/src/429.n-叉树的层序遍历.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=429 lang=java
+ *
+ * [429] N 叉树的层序遍历
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        if (root == null) {
+            return new ArrayList<List<Integer>>();
+        }
+
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        Queue<Node> queue = new ArrayDeque<Node>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int cnt = queue.size();
+            List<Integer> level = new ArrayList<Integer>();
+            for (int i = 0; i < cnt; ++i) {
+                Node cur = queue.poll();
+                level.add(cur.val);
+                for (Node child : cur.children) {
+                    queue.offer(child);
+                }
+            }
+            ans.add(level);
+        }
+
+        return ans;
+    }
+}
+// @lc code=end
+
