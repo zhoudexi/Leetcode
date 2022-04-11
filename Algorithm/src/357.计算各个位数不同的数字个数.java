34c5c562af21db36cb431ac014a4909a857c4df8
zhoudexi <1192132727@qq.com>
2022-04-11 23:22:05 +0800

    提交

---
+++ Algorithm/src/357.计算各个位数不同的数字个数.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=357 lang=java
+ *
+ * [357] 计算各个位数不同的数字个数
+ */
+
+// @lc code=start
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 10;
+        }
+        int res = 10, cur = 9;
+        for (int i = 0; i < n - 1; i++) {
+            cur *= 9 - i;
+            res += cur;
+        }
+        return res;
+    }
+}
+
+// @lc code=end
+
