4e7875b2e0390b7883b483f40d679efad7091bf3
zhoudexi <1192132727@qq.com>
2022-05-08 23:32:03 +0800

    提交

---
+++ Algorithm/src/442.数组中重复的数据.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=442 lang=java
+ *
+ * [442] 数组中重复的数据
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        List<Integer> ans = new ArrayList<Integer>();
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] - 1 != i) {
+                ans.add(nums[i]);
+            }
+        }
+        return ans;
+    }
+
+    public void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+}
+
+// @lc code=end
+
