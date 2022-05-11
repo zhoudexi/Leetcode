3c6ddf8ae60cf859e696f85a5f5b44f3e567e4d5
zhoudexi <1192132727@qq.com>
2022-05-11 23:33:08 +0800

    提交

---
+++ Algorithm/src/449.序列化和反序列化二叉搜索树.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=449 lang=java
+ *
+ * [449] 序列化和反序列化二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+    public String serialize(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        postOrder(root, list);
+        String str = list.toString();
+        return str.substring(1, str.length() - 1);
+    }
+
+    public TreeNode deserialize(String data) {
+        if (data.isEmpty()) {
+            return null;
+        }
+        String[] arr = data.split(", ");
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        int length = arr.length;
+        for (int i = 0; i < length; i++) {
+            stack.push(Integer.parseInt(arr[i]));
+        }
+        return construct(Integer.MIN_VALUE, Integer.MAX_VALUE, stack);
+    }
+
+    private void postOrder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left, list);
+        postOrder(root.right, list);
+        list.add(root.val);
+    }
+
+    private TreeNode construct(int lower, int upper, Deque<Integer> stack) {
+        if (stack.isEmpty() || stack.peek() < lower || stack.peek() > upper) {
+            return null;
+        }
+        int val = stack.pop();
+        TreeNode root = new TreeNode(val);
+        root.right = construct(val, upper, stack);
+        root.left = construct(lower, val, stack);
+        return root;
+    }
+}
+
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// String tree = ser.serialize(root);
+// TreeNode ans = deser.deserialize(tree);
+// return ans;
+// @lc code=end
+
