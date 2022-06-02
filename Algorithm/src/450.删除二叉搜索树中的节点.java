3593dd2b763d95ac297914ea7e01a0eee0c4aee2
zhoudexi <1192132727@qq.com>
2022-06-02 23:06:17 +0800

    提交

---
+++ Algorithm/src/450.删除二叉搜索树中的节点.java
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=450 lang=java
+ *
+ * [450] 删除二叉搜索树中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        if (root.val == key) {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+            TreeNode t = root.right;
+            while (t.left != null) t = t.left;
+            t.left = root.left;
+            return root.right;
+        } else if (root.val < key) root.right = deleteNode(root.right, key);
+        else root.left = deleteNode(root.left, key);
+        return root;
+    }
+}
+// @lc code=end
+
