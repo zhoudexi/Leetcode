bfd796795037b04fc0ad4fcfb0afbf8bd25948b1
zhoudexi <1192132727@qq.com>
2022-06-08 23:48:30 +0800

    提交

---
+++ Algorithm/src/1037.有效的回旋镖.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1037 lang=java
+ *
+ * [1037] 有效的回旋镖
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isBoomerang(int[][] ps) {
+        return (ps[1][0] - ps[0][0]) * (ps[2][1] - ps[0][1]) != (ps[2][0] - ps[0][0]) * (ps[1][1] - ps[0][1]);
+    }
+}
+// @lc code=end
+
