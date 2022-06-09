69615c64012be595d730aa635f24075d7e4d8f9e
zhoudexi <1192132727@qq.com>
2022-06-09 23:27:01 +0800

    提交

--- Algorithm/src/1037.有效的回旋镖.java
+++ Algorithm/src/1037.有效的回旋镖.java
@@ -5,10 +5,10 @@
  */
 
 // @lc code=start
-class Solution {
-    public boolean isBoomerang(int[][] ps) {
-        return (ps[1][0] - ps[0][0]) * (ps[2][1] - ps[0][1]) != (ps[2][0] - ps[0][0]) * (ps[1][1] - ps[0][1]);
+    class Solution {
+        public boolean isBoomerang(int[][] ps) {
+            return (ps[1][0] - ps[0][0]) * (ps[2][1] - ps[0][1]) != (ps[2][0] - ps[0][0]) * (ps[1][1] - ps[0][1]);
+        }
     }
-}
 // @lc code=end
 
---
+++ Algorithm/src/497.非重叠矩形中的随机点.java
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=497 lang=java
+ *
+ * [497] 非重叠矩形中的随机点
+ */
+
+// @lc code=start
+class Solution {
+    Random rand;
+    List<Integer> arr;
+    int[][] rects;
+
+    public Solution(int[][] rects) {
+        rand = new Random();
+        arr = new ArrayList<Integer>();
+        arr.add(0);
+        this.rects = rects;
+        for (int[] rect : rects) {
+            int a = rect[0], b = rect[1], x = rect[2], y = rect[3];
+            arr.add(arr.get(arr.size() - 1) + (x - a + 1) * (y - b + 1));
+        }
+    }
+
+    public int[] pick() {
+        int k = rand.nextInt(arr.get(arr.size() - 1));
+        int rectIndex = binarySearch(arr, k + 1) - 1;
+        k -= arr.get(rectIndex);
+        int[] rect = rects[rectIndex];
+        int a = rect[0], b = rect[1], y = rect[3];
+        int col = y - b + 1;
+        int da = k / col;
+        int db = k - col * da;
+        return new int[]{a + da, b + db};
+    }
+
+    private int binarySearch(List<Integer> arr, int target) {
+        int low = 0, high = arr.size() - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int num = arr.get(mid);
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(rects);
+ * int[] param_1 = obj.pick();
+ */
+// @lc code=end
+
